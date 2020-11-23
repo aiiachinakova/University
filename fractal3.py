44b0f99f6ade1606de6b2f355aac57f642729e93
aiiachinakova <71030693+aiiachinakova@users.noreply.github.com>
2020-11-24 00:10:20 +0300

    Add files via upload

---
+++ fractal3.py
@@ -0,0 +1,23 @@
+import turtle as tl
+
+def draw_fractal(scale):
+    if scale >= 5:
+        draw_fractal(scale / 3.0)
+        tl.left(90)
+        draw_fractal(scale / 3.0)
+        tl.right(90)
+        draw_fractal(scale / 3.0)
+        tl.right(90)
+        draw_fractal(scale / 3.0)
+      
+    else:
+        tl.forward(scale)
+
+scale = 400
+tl.pensize(2)
+tl.penup()
+tl.goto(-scale, -scale/4)
+tl.pendown()
+
+draw_fractal(scale)
+tl.done()
